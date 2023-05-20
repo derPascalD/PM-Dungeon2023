35f1b6e8a7dab84daa78921d29991b029e6ae23f
Simon Gartzke <simon.gartzke@icloud.com>
2023-05-20 16:05:54 +0200

    Implemented HealQuest

--- game/src/ecs/quest/HealQuest.java
+++ game/src/ecs/quest/HealQuest.java
@@ -1,6 +1,13 @@
 package ecs.quest;
 
+import ecs.components.HealthComponent;
+import ecs.entities.Hero;
+import ecs.items.ImplementedItems.Healthpot;
+import starter.Game;
+
 public class HealQuest extends Quest {
+
+    private int questNumber = 10;
 
     public HealQuest(String name, String description) {
         super(name, description);
@@ -8,16 +15,20 @@ public class HealQuest extends Quest {
 
     @Override
     public String getProgress() {
-        return null;
+        return Healthpot.getUseCount() + "/" + questNumber + " Healthpots used";
     }
 
     @Override
     public boolean isComplete() {
+        if(Healthpot.getUseCount()>=questNumber) return true;
         return false;
     }
 
     @Override
     public void onComplete() {
-
+        Hero hero = (Hero) Game.getHero().get();
+        HealthComponent heroHealth =
+            (HealthComponent) hero.getComponent(HealthComponent.class).get();
+        heroHealth.setMaximalHealthpoints(heroHealth.getMaximalHealthpoints()+10);
     }
 }
