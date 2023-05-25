48dc04b172d0f0f4434f980c74f221e92a1b744c
derPascalD <pascal2122@gmx.de>
2023-05-25 13:03:40 +0200

    Add new HealingSystem in Game Classin the Method createSystems()

--- game/src/starter/Game.java
+++ game/src/starter/Game.java
@@ -428,5 +428,6 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
         new SkillSystem();
         new ProjectileSystem();
         new QuestSystem();
+        new HealingSystem();
     }
 }
