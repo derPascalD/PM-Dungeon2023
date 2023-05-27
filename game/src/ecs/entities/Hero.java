f46a42ffdb998172164d9692fc4d3e91dca5e2eb
derPascalD <pascal2122@gmx.de>
2023-05-27 16:32:03 +0200

    Add HealingComponent in Hero with new Parameter

--- game/src/ecs/entities/Hero.java
+++ game/src/ecs/entities/Hero.java
@@ -64,7 +64,7 @@ public class Hero extends Entity implements IOnDeathFunction, ILevelUp, ICollide
         super();
         playableComponent = new PlayableComponent(this);
         new PositionComponent(this);
-        new HealingComponent(this,2,10);
+        new HealingComponent(this, 10, 2, 3);
         killedMonsters = new ArrayList<>();
         setupVelocityComponent();
         setupAnimationComponent();
