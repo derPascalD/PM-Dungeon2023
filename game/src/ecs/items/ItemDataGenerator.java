e0ca631e168a9fa41bed7b0afbcfc9fac83312e2
Oliver Adler <oliver_elias.adler@fh-bielefeld.de>
2023-06-06 23:40:38 +0200

    Reset width and height

--- game/src/ecs/items/ItemDataGenerator.java
+++ game/src/ecs/items/ItemDataGenerator.java
@@ -1,11 +1,9 @@
 package ecs.items;
 
-import dslToGame.AnimationBuilder;
 import ecs.items.ImplementedItems.Chestplate;
 import ecs.items.ImplementedItems.Healthpot;
 import ecs.items.ImplementedItems.SimpleWand;
 import graphic.Animation;
-
 import java.util.List;
 import java.util.Random;
 
@@ -17,29 +15,28 @@ public class ItemDataGenerator {
     private static final List<String> chestplate = List.of("items/chestplate/chestplate.png");
     private static final List<String> simplWand = List.of("items/simplewand/simplewand.png");
 
-
     private List<ItemData> templates =
             List.of(
                     new Healthpot(
                             ItemType.Healing,
-                            new Animation(healtpot,1),
-                            new Animation(healtpot,1),
-                           // AnimationBuilder.buildAnimation(healtpot),
-                            //AnimationBuilder.buildAnimation(healtpot),
+                            new Animation(healtpot, 1),
+                            new Animation(healtpot, 1),
+                            // AnimationBuilder.buildAnimation(healtpot),
+                            // AnimationBuilder.buildAnimation(healtpot),
                             "Healthpot",
                             "Heals the Player on Use"),
                     new SimpleWand(
                             ItemType.Weapon,
-                            new Animation(simplWand,1),
-                            new Animation(simplWand,1),
-                           // AnimationBuilder.buildAnimation(simplWand),
-                           //AnimationBuilder.buildAnimation(simplWand),
+                            new Animation(simplWand, 1),
+                            new Animation(simplWand, 1),
+                            // AnimationBuilder.buildAnimation(simplWand),
+                            // AnimationBuilder.buildAnimation(simplWand),
                             "SimpleWand",
                             "Gives the Player more Attack Damage"),
                     new Chestplate(
                             ItemType.Armor,
-                            new Animation(chestplate,1),
-                            new Animation(chestplate,1),
+                            new Animation(chestplate, 1),
+                            new Animation(chestplate, 1),
                             //// AnimationBuilder.buildAnimation(armor),
                             "Chestplate",
                             "Protects the Player"));
