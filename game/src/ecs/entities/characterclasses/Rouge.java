af77f5fc4fdd88e4bf1e58b5951cebc219a8bd68
Simon Gartzke <simon.gartzke@icloud.com>
2023-06-15 00:30:42 +0200

    added rouge assets

--- game/src/ecs/entities/characterclasses/Rouge.java
+++ game/src/ecs/entities/characterclasses/Rouge.java
@@ -1,9 +1,12 @@
 package ecs.entities.characterclasses;
 
+import dslToGame.AnimationBuilder;
+import ecs.components.AnimationComponent;
 import ecs.components.DamageComponent;
 import ecs.components.HealthComponent;
 import ecs.components.VelocityComponent;
 import ecs.entities.Hero;
+import graphic.Animation;
 
 /** Represents the Rouge Class */
 public class Rouge extends Hero {
@@ -17,11 +20,21 @@ public class Rouge extends Hero {
         new HealthComponent(this, 15, this, hitAnimation(), hitAnimation());
         new DamageComponent(this, 4, 0);
         setupVelocity();
+        setupAnimation();
     }
 
     private void setupVelocity() {
+        Animation moveLeft = AnimationBuilder.buildAnimation("rouge/idleCombatLeft");
+        Animation moveRight = AnimationBuilder.buildAnimation("rouge/idleCombatRight");
         new VelocityComponent(this, 0.4f, 0.4f, moveLeft, moveRight);
     }
+
+    private void setupAnimation() {
+        Animation idleLeft = AnimationBuilder.buildAnimation("rouge/idleCombatLeft");
+        Animation idleRight = AnimationBuilder.buildAnimation("rouge/idleCombatRight");
+        new AnimationComponent(this,idleLeft,idleRight);
+    }
+
 
     private void setupSkills() {
         setupMeleeSkill();
